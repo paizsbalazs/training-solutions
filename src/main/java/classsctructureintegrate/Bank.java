ce347db7f6c7252f2a1441528a3ffac68f3eb343
paizsbalazs <GdecodeRPaizs1983>
2020-11-01 17:12:30 +0100

    First commit

---
+++ src/main/java/classsctructureintegrate/Bank.java
@@ -0,0 +1,54 @@
+package classsctructureintegrate;
+
+import java.util.Scanner;
+
+public class Bank {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Milyen néven szertné megnyitni a bankszámlaszám1 - et?");
+        String name1 = scanner.nextLine();
+
+        System.out.println("Mi lesz a bankszámlaszám1?");
+        String bnkszla1 = scanner.nextLine();
+
+        System.out.println("Mi lesz a bankszámlaszám1 nyitóegyenlege?");
+        Integer nyito1 = scanner.nextInt();
+
+        BankAccount bankAccount = new BankAccount(bnkszla1,name1, nyito1);
+
+        System.out.println("Milyen néven szertné megnyitni a bankszámlaszám2 - et?");
+
+        String name2 = scanner.nextLine();
+
+        System.out.println("Mi lesz a bankszámlaszám2?");
+        String bnkszla2 = scanner.nextLine();
+
+        System.out.println("Mi lesz a bankszámlaszám2 nyitóegyenlege?");
+        Integer nyito2 = scanner.nextInt();
+
+        BankAccount anotherbankAccount = new BankAccount(bnkszla2,name2, nyito2);
+
+        System.out.println(bankAccount.getInfo());
+        System.out.println(anotherbankAccount.getInfo());
+
+        System.out.println("Mennyit szeretne befizetni?");
+        Integer depamount = scanner.nextInt();
+        bankAccount.deposit(depamount);
+        System.out.println(bankAccount.getInfo());
+
+        System.out.println("Mennyit szeretne Kivenni?");
+        Integer wamount = scanner.nextInt();
+        anotherbankAccount.withdraw(wamount);
+        System.out.println(bankAccount.getInfo());
+        System.out.println(anotherbankAccount.getInfo());
+
+        System.out.println("Mennyit szeretne Utalni?");
+        Integer pamount = scanner.nextInt();
+        bankAccount.transfer(anotherbankAccount, pamount);
+
+        System.out.println(bankAccount.getInfo());
+        System.out.println(anotherbankAccount.getInfo());
+    }
+}
---
+++ src/main/java/classsctructureintegrate/BankAccount.java
@@ -0,0 +1,44 @@
+package classsctructureintegrate;
+
+public class BankAccount {
+
+    private String AccountNumber;
+    private String Owner;
+    private Integer Balance;
+
+    public BankAccount(String accountNumber, String owner, Integer balance) {
+        this.AccountNumber = accountNumber;
+        this.Owner = owner;
+        this.Balance = balance;
+    }
+
+    public String getAccountNumber() {
+        return AccountNumber;
+    }
+
+    public String getOwner() {
+        return Owner;
+    }
+
+    public Integer getBalance() {
+        return Balance;
+    }
+
+    public void deposit(Integer amount) {
+        this.Balance = this.Balance + amount;
+    }
+
+    public void withdraw(Integer amount) {
+        this.Balance = this.Balance - amount;
+    }
+
+    public void transfer(BankAccount to, Integer amount) {
+        this.withdraw(amount);
+        to.deposit(amount);
+    }
+
+    public String getInfo() {
+        return this.getOwner() + " (" + this.getAccountNumber() + "): " + this.getBalance() + " Ft";
+    }
+
+}
