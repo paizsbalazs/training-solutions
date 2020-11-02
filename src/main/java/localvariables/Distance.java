c0e9520a901b1523f7c052f4fd8faa623e5431e5
paizsbalazs <GdecodeRPaizs1983>
2020-11-02 21:03:23 +0100

    First commit

---
+++ src/main/java/localvariables/Distance.java
@@ -0,0 +1,15 @@
+package localvariables;
+
+public class Distance {
+
+    private double distanceInKm = 1600;
+    private boolean exact = true;
+
+    public double getDistanceInKm() {
+        return distanceInKm;
+    }
+
+    public boolean isExact() {
+        return exact;
+    }
+}
---
+++ src/main/java/localvariables/DistanceMain.java
@@ -0,0 +1,16 @@
+package localvariables;
+
+public class DistanceMain {
+
+    public static void main(String[] args) {
+
+        Distance distance = new Distance();
+
+        System.out.println(distance.getDistanceInKm() + " / " + distance.isExact() );
+
+        //int partOfDistamceKm = distance.getDistanceInKm();
+
+        //System.out.println(partOfDistamceKm);
+
+    }
+}
