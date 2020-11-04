2420ecf683044cdfc5eff293991038a094a48621
paizsbalazs <GdecodeRPaizs1983>
2020-11-04 13:51:11 +0100

    First commit

---
+++ src/main/java/javaapi/Upper.java
@@ -0,0 +1,9 @@
+package javaapi;
+
+public class Upper {
+
+    public static void main(String[] args) {
+        String gretting = new String("Hello World!");
+        System.out.println(gretting.toUpperCase());
+    }
+}
