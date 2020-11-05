b128ba8cb877234fe73b2d3e94ef2003e5bec71b
paizsbalazs <GdecodeRPaizs1983>
2020-11-05 15:51:02 +0100

    First commit

---
+++ src/main/java/week02/InputNames.java
@@ -0,0 +1,25 @@
+package week02;
+
+import java.lang.reflect.Array;
+import java.util.Scanner;
+
+public class InputNames {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        String[] arrayOfNames = new String[5];
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Kérem szépen az első nevet");
+            arrayOfNames[i] = scanner.nextLine();
+
+        }
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println(arrayOfNames[i]);
+
+        }
+    }
+
+}
