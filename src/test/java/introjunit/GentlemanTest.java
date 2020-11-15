daaba95b026bf853c110079dc5c19690c36da2aa
paizsbalazs <GdecodeRPaizs1983>
2020-11-15 16:44:05 +0100

    First commit

---
+++ src/test/java/introjunit/GentlemanTest.java
@@ -0,0 +1,24 @@
+package introjunit;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class GentlemanTest {
+
+
+    @Test
+    void byNormalName() {
+
+        //Given
+        String name = "John Doe";
+
+        //When
+        String greeting = new Gentleman().sayHello(name);
+
+        //Then
+        assertEquals("Hello John Doe", greeting);
+    }
+
+}
