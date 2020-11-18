d8e1bd4354c82e1a15876004df1efe7b34ce9879
paizsbalazs <GdecodeRPaizs1983>
2020-11-18 15:47:36 +0100

    First commit

---
+++ src/main/java/week04/NumberGuesser.java
@@ -0,0 +1,30 @@
+package week04;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class NumberGuesser {
+
+    public static void main(String[] args) {
+
+        int randomNumber = new Random().nextInt(100);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Gondoltam egy számra egy és száz között, kérem a tippet!");
+        int tipp = scanner.nextInt();
+
+        while ( tipp != randomNumber ) {
+            if (tipp<randomNumber) {
+                System.out.println("Kisebb a tipp, mint a gondolt szám");
+                System.out.println("Következő tipp!");
+                tipp = scanner.nextInt();
+            } else {
+                System.out.println("Nagyobb a tipp, mint a gondolt szám");
+                System.out.println("Következő tipp!");
+                tipp = scanner.nextInt();
+            }
+        }
+
+        System.out.println("A két szám megegyezik!");
+
+    }
+}
