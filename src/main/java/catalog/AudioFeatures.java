c403ea89fc52edd7ec5efb5ea8a56555e5554888
paizsbalazs <GdecodeRPaizs1983>
2021-01-06 18:04:48 +0100

    First commit

---
+++ src/main/java/catalog/AudioFeatures.java
@@ -0,0 +1,54 @@
+package catalog;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AudioFeatures implements Feature{
+
+   private final List<String> composer;
+   private final int length;
+   private final List<String> performers;
+   private final String title;
+
+    public int getLength() {
+        return length;
+    }
+
+    public AudioFeatures(String title, int length, List<String> composer, List<String> performers) {
+        this.composer = composer;
+        this.length = length;
+        this.performers = performers;
+        this.title = title;
+    }
+
+    public AudioFeatures(String title, int length, List<String> composer) {
+        this.composer = composer;
+        this.length = length;
+        this.performers = null;
+        this.title = title;
+    }
+
+    @Override
+    public List<String> getContributors() {
+
+        List<String> result = new ArrayList<>();
+
+        if (this.performers.size()>0) {
+
+            for (int i = 0; i < this.performers.size(); i++) {
+                result.add(this.performers.get(i).toString());
+            }
+        }
+
+        for (int j = 0; j < this.composer.size(); j++) {
+            result.add(this.composer.get(j).toString());
+        }
+
+        return result;
+    }
+
+    @Override
+    public String getTitle() {
+        return title;
+    }
+}
---
+++ src/test/java/catalog/AudioFeaturesTest.java
@@ -0,0 +1,34 @@
+package catalog;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AudioFeaturesTest {
+
+    @Test
+    public void testCreate() {
+
+        AudioFeatures audioFeatures = new AudioFeatures("Ride the Lightning", 300, Arrays.asList("Metallica"));
+
+        assertEquals("Ride the Lightning", audioFeatures.getTitle());
+        assertEquals(300, audioFeatures.getLength());
+
+        assertEquals(1, audioFeatures.getContributors().size());
+        assertEquals("Metallica", audioFeatures.getContributors().get(0));
+    }
+
+    @Test
+    public void testCreateWithComposer() {
+
+        AudioFeatures audioFeatures = new AudioFeatures("Ride the Lightning", 300, Arrays.asList("Metallica"), Arrays.asList("James Hetfield"));
+
+        assertEquals("Ride the Lightning", audioFeatures.getTitle());
+        assertEquals(300, audioFeatures.getLength());
+
+        assertEquals(2, audioFeatures.getContributors().size());
+        assertEquals("Metallica", audioFeatures.getContributors().get(1));
+    }
+}
