47aeee3572a929daa02a55fce6ab97ba3d048d65
paizsbalazs <GdecodeRPaizs1983>
2021-01-10 16:37:47 +0100

    First commit

--- src/main/java/exam02/Cv.java
+++ src/main/java/exam02/Cv.java
@@ -3,6 +3,8 @@ package exam02;
 import java.util.ArrayList;
 import java.util.List;
 
+import static java.lang.Integer.parseInt;
+
 public class Cv {
 
     private String name;
@@ -16,13 +18,29 @@ public class Cv {
         return name;
     }
 
-    public void addSkills(Skill... skill) {
+    public void addSkills(String... skill) {
         for (int i = 0; i < skill.length; i++ ) {
-            this.skills.add(skill[i]);
+            this.skills.add(new Skill(skill[i].substring(0, skill[i].indexOf("(")-1), parseInt(skill[i].substring(skill[i].indexOf("(")+1, skill[i].length()-1))));
         }
     }
 
     public List<Skill> getSkills() {
         return skills;
+    }
+
+    public int findSkillLevelByName(String kskill) {
+        int result=0;
+
+        for (Skill s: this.skills) {
+            if (s.getName().equals(kskill)) {
+                result = s.getLevel();
+            }
+        }
+
+        if (result==0) {
+            throw new IllegalArgumentException(kskill);
+        }
+
+        return result;
     }
 }
