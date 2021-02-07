56a60f721b2b696625bb704ad4cc3a857c34f160
paizsbalazs <GdecodeRPaizs1983>
2021-02-07 11:00:44 +0100

    First commit

---
+++ src/main/java/week14d03/ClassNoteBook.java
@@ -0,0 +1,24 @@
+package week14d03;
+
+import java.util.*;
+
+public class ClassNoteBook {
+
+    private List<Student> students;
+
+    public ClassNoteBook(List<Student> students) {
+        this.students = students;
+    }
+
+    public List<Student> sortNoteBook() {
+        List<Student> result = new ArrayList<>(students);
+        Collections.sort(result);
+        return result;
+    }
+
+    public List<Student> getStudents() {
+        return students;
+    }
+
+
+}
