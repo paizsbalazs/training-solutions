787b57445279ca1c23cee4d7a5ccbde33741bf0e
paizsbalazs <GdecodeRPaizs1983>
2021-02-15 17:43:19 +0100

    First commit

---
+++ src/main/java/exam03/Cruise.java
@@ -0,0 +1,21 @@
+package exam03;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cruise {
+
+    private Boat boat;
+    private LocalDate sailing;
+    private double basicPrice;
+    private List<Passenger> passengers = new ArrayList<>();
+
+    public Cruise(Boat boat, LocalDate sailing, double basicPrice) {
+        this.boat = boat;
+        this.sailing = sailing;
+        this.basicPrice = basicPrice;
+    }
+
+
+}
