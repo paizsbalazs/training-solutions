d5c5a161765e2dabc9efae79af2d303ee4627f6b
paizsbalazs <GdecodeRPaizs1983>
2021-02-15 18:00:39 +0100

    First commit

--- src/main/java/exam03/Cruise.java
+++ src/main/java/exam03/Cruise.java
@@ -17,5 +17,43 @@ public class Cruise {
         this.basicPrice = basicPrice;
     }
 
+    public Boat getBoat() {
+        return boat;
+    }
+
+    public LocalDate getSailing() {
+        return sailing;
+    }
+
+    public double getBasicPrice() {
+        return basicPrice;
+    }
+
+    public List<Passenger> getPassengers() {
+        return passengers;
+    }
+
+    public void bookPassenger(Passenger passenger) {
+
+        if (boat.getMaxPassengers()<=this.passengers.size()+1) {
+            passengers.add(passenger);
+        } else {
+            throw new IllegalArgumentException("");
+        }
+
+    }
+
+    public double getPriceForPassenger(Passenger passenger) {
+        double price = 0.0;
+        if (passenger.getCruiseClass().equals(CruiseClass.FIRST)) {
+            price = 1.8 * this.getBasicPrice();
+        } else {
+            if (passenger.getCruiseClass().equals(CruiseClass.LUXURY)) {
+                price = 3.0 * this.getBasicPrice();
+            }
+            price = this.basicPrice;
+        }
+        return price;
+    }
 
 }
