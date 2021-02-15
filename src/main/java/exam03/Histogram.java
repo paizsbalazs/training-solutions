26e312b5cc09de94c065f04bf364b3eb79455a9d
paizsbalazs <GdecodeRPaizs1983>
2021-02-15 17:20:59 +0100

    First commit

--- src/main/java/exam03/Histogram.java
+++ src/main/java/exam03/Histogram.java
@@ -1,5 +1,40 @@
 package exam03;
 
+import week15d02.Question;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 public class Histogram {
+
+    public String createHistogram(BufferedReader reader) {
+        StringBuilder sb = new StringBuilder();
+        String line;
+
+        try {
+            while ((line = reader.readLine()) != null) {
+                for (int i = 0; i<Integer.parseInt(line); i++) {
+                    sb.append("*");
+                }
+                sb.append("\n");
+            }
+        } catch (IOException e) {
+            throw new IllegalArgumentException("File read error");
+        }
+
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        Path path = Path.of("histogram.txt");
+        try (BufferedReader br = Files.newBufferedReader(path)) {
+            Histogram histogram = new Histogram();
+            System.out.println(histogram.createHistogram(br));
+        } catch (IOException ioe) {
+            throw new IllegalStateException("Can not read the file", ioe);
+        }
+    }
 
 }
