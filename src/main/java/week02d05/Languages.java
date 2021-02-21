fec98184f4abca0c34029c0207c8f7f7f80bb7b4
paizsbalazs <GdecodeRPaizs1983>
2021-02-21 08:09:43 +0100

    rename

---
+++ src/main/java/week02d05/Languages.java
@@ -0,0 +1,24 @@
+package week02d05;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Languages {
+
+    public static void main(String[] args) {
+
+        List<String> languages = new ArrayList<>();
+        languages.add("Java");
+        languages.add("Python");
+        languages.add("JavaScript");
+
+       // for(String languages: languages) {
+        //if (languages.length() < 5) {
+          //      System.out.println(languages);
+           //}
+       //   }
+    }
+
+
+}
