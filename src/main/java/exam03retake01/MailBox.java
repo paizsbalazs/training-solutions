d507184a2fb91b24a4eeea128252a4b49e522b23
paizsbalazs <GdecodeRPaizs1983>
2021-02-22 17:19:46 +0100

    rename

---
+++ src/main/java/exam03retake01/MailBox.java
@@ -0,0 +1,54 @@
+package exam03retake01;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MailBox {
+
+    private List<Mail> mails = new ArrayList<>();
+
+    public void addMail(Mail mail) {
+        mails.add(mail);
+    }
+
+    public List<Mail> getMails() {
+        return mails;
+    }
+
+    public List<Mail> findByCriteria(String criteria) {
+        String[] fields = criteria.split(":");
+        List<Mail> result = new ArrayList<>();
+
+        if (fields[0].equals("from")) {
+            for (Mail m: mails ) {
+                if (fields[1].equals(m.getFrom().getEmail())) {
+                    result.add(m);
+                }
+            }
+        }
+
+        if (fields[0].equals("to")) {
+            for (Mail m: mails ) {
+
+                for (int i = 0; i<m.getTo().size(); i++) {
+                    if (m.getTo().get(i).getName().equals(fields[1])) {
+                        result.add(m);
+                    }
+                }
+
+            }
+        } else {
+
+            for (Mail m: mails ) {
+                if (fields[1].equals(m.getSubject())) {
+                    result.add(m);
+                }
+            }
+
+        }
+
+        return result;
+    }
+
+
+}
